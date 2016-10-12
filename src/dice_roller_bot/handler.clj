28a87f44e2ea94aed64a0deefef5b8be12145361
Bruno Assis <brunoassis@gmail.com>
2016-10-11 23:38:45 -0300

    Added Bot Webhook reply

--- src/dice_roller_bot/handler.clj
+++ src/dice_roller_bot/handler.clj
@@ -1,25 +1,47 @@
 (ns dice-roller-bot.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.json :refer [wrap-json-response]]
-            [ring.util.response :refer [response]]))
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+            [ring.util.response :refer [response]]
+            [clojure.string :refer [join]]))
 
 (defn roll-dice [faces]
   (case faces
     "f" (rand-nth [-1 0 1])
     (inc (rand-int (Integer/parseInt faces)))))
 
-(defn dice-response [quantity faces]
-  (let [throws (into [] (repeatedly quantity #(roll-dice faces)))] 
-    (response {:quantity quantity
-               :faces faces
-               :throws throws
-               :sum (reduce + throws)})))
+(defn dice [quantity faces]
+  (let [quantity (Integer/parseInt quantity)
+        throws (into [] (repeatedly quantity #(roll-dice faces)))] 
+    {:quantity quantity
+     :faces faces
+     :throws throws
+     :sum (reduce + throws)}))
+
+(defn generate-dice-message [dice-text]
+  (let [captures (re-find #"/(\d+)d(.*?)$" dice-text)
+        quantity (get captures 1)
+        faces (get captures 2)
+        dice (dice quantity faces)
+        throws (get dice :throws)
+        throws-text (join " + " throws)
+        sum (get dice :sum)]
+    (str "ROLL " quantity "d" faces ": [" throws-text "] = " sum)))
+
+(defn generate-bot-reply [message]
+  {:method "sendMessage"
+   :text (generate-dice-message (get-in message ["message" "text"]))
+   :chat_id (get-in message ["message" "chat" "id"])
+   :reply_to_message_id (get-in message ["message" "message_id"])})
 
 (defroutes app-routes
+  (POST "/botWebhook" {message :body}
+        (response (generate-bot-reply message)))
   (GET "/dice/:quantity/:faces" [quantity faces]
-       (dice-response (Integer/parseInt quantity) faces))
+       (response (dice quantity faces)))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-json-response app-routes))
+  (-> app-routes
+      wrap-json-response
+      wrap-json-body))
